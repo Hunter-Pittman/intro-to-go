252d3d67910d2c623fbcfca12fa444446bbc1893
Hunter Pittman <hunterpittman604@gmail.com>
2023-02-17 11:33:24 -0800

    Added pointer lesson Added user input Added basic logic

--- main.go
+++ main.go
@@ -2,41 +2,67 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
 
-	// There are three main types of ways to declare a variable in go
+	var eventName = "CISO Super Cyber Summit"
+	const totalTickets = 50
+	var remainingTickets = 50
+	fmt.Printf("Welcome to the %v! \n", eventName)
+	fmt.Printf("There are %v tickets remainning at %s \n", remainingTickets, eventName)
 
-	// The first way
-	var firstName string
-	firstName = "Hunter"
+	// & creates a pointer (sometimes called a borrow) and * gets the value of a pointer (also called a dereference)
 
-	// The second way
-	lastName := "Pittman"
+	randomValue1 := "bruh"
+	var memoryAddressOfRandomValue1 *string
+	memoryAddressOfRandomValue1 = &randomValue1
 
-	// The third way
+	fmt.Printf("The memory address of randomValue1 is %v \n", memoryAddressOfRandomValue1)
+
+	infoGathering(totalTickets, remainingTickets)
+
+}
+
+func infoGathering(totalTickets int, remainingTickets int) {
 	var (
-		id string
-		//totalMoney float32
-		time = time.Now()
+		id          string
+		firstName   string
+		lastName    string
+		email       string
+		userTickets int
 	)
 
 	fmt.Print("Enter your ID: ")
-	fmt.Scanln(&id)
+	fmt.Scan(&id)
 
-	//fmt.Print
+	fmt.Print("Enter your first name: ")
+	fmt.Scan(&firstName)
 
+	fmt.Print("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Print("Enter your email: ")
+	fmt.Scan(&email)
+
+	fmt.Print("Enter the number of tickets you want: ")
+	fmt.Scan(&userTickets)
+
+	if userTickets > remainingTickets {
+		fmt.Println("I am sorry there are not enough tickets left to complete your transaction. Please try again later. Ending program!")
+		os.Exit(0)
+	} else if userTickets == remainingTickets {
+		fmt.Println("Congratualtions! You have purchased the last ticket for this event! Ending program!")
+		os.Exit(0)
+	} else {
+		remainingTickets -= userTickets
+		fmt.Printf("There are %v tickets left for this event. \n", remainingTickets)
+	}
+
+	fmt.Println("###########Transaction Information###########")
 	fmt.Printf("Your ID: %v \n", id)
-
 	fmt.Printf("Your Name: %s \n", firstName+" "+lastName)
-
-	fmt.Printf("The Current Time: %s \n", time)
-	goodbye()
-}
-
-func goodbye() {
-	fmt.Println("Goodbye, World!")
+	fmt.Printf("Your email: %s \n", email)
+	fmt.Printf("Number of tickets purchased: %v \n", userTickets)
 }
